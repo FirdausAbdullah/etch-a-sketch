21ad1198b8b4747c9e6371501a12d9af788dd0c4
ddoabdullahXubuntu <mfirdaus.tch@gmail.com>
2023-10-10 15:40:31 +0800

    Add proper 16x16 grid
    Manage to create a proper 16 by 16 grid with
    drawGrid() function using JS. Also added a
    toolbox section for tool selection for sketching.

--- script.js
+++ script.js
@@ -2,21 +2,30 @@
 Etch-a-Sketch project
 TOP prescribed project
 */
-function drawPad (pixelRow, pixelColumn){
+function makePixel (pixelRow, pixelColumn){
     const pixelContainer = document.querySelector('.pixel-container');
     const pixel = document.createElement('div');
-    pixel.style.width = '10px';
-    pixel.style.height = '10px';
+    let row = (pixelContainer.clientHeight)/pixelRow;
+    let column = (pixelContainer.clientWidth)/pixelColumn;
+    pixel.style.width = `${column}px`;
+    pixel.style.height = `${row}px`;
     pixel.style.border = '1px solid black';
     pixel.className = 'pixel';
-
     pixelContainer.appendChild(pixel);
 }
 
-for (let i =0; i<320; i++){
-    drawPad();
+function drawGrid(row,column){
+    for(let i=0; i<(row*column);i++){
+        makePixel(row,column);
+    }
+
 }
 
+
+
+
+
+drawGrid(16,16);
 
 
 
