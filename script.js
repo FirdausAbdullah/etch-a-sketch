078029edaf16868df86e0b047b503a34a923ace7
ddoabdullahXubuntu <mfirdaus.tch@gmail.com>
2023-10-10 14:04:39 +0800

    Make a div with 16div in a line
    Still not a 16x16. Need to figure out the sizing
    that is going to be used.

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 Etch-a-Sketch project
 TOP prescribed project
 */
+function drawPad (pixelRow, pixelColumn){
+    const pixelContainer = document.querySelector('.pixel-container');
+    const pixel = document.createElement('div');
+    pixel.style.width = '10px';
+    pixel.style.height = '10px';
+    pixel.style.border = '1px solid black';
+    pixel.className = 'pixel';
+
+    pixelContainer.appendChild(pixel);
+}
+
+for (let i =0; i<320; i++){
+    drawPad();
+}
+
+
+
+
