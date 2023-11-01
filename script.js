0ccc162c993f45d8dd87bd1458301fef960078e5
FirdausAbdullah <mfirdaus.tch@gmail.com>
2023-11-01 20:13:17 +0800

    Add clear button functionality  Clicking clear button will clear all pixel.
    Add input for pixel drawing dimension
     inputting a number in this field will result
     in the drawing pad to be redrawn with the new
     number*number dimension.

--- script.js
+++ script.js
@@ -1,57 +1,50 @@
-/*
-Etch-a-Sketch project
-TOP prescribed project
-*/
+const container = document.querySelector('.pixel-container');
+const sizeInput = document.querySelector('.size');
+const color = document.querySelector('.color');
+const resetBtn = document.querySelector('.reset');
 
+let size = sizeInput.value;
 
+let draw = false;
 
-const inputColor = document.querySelector('.color');
-inputColor.addEventListener('input',(e)=>{currentColor = e.target.value;});
-let currentColor = inputColor.value;
+//set size of grid area (size*size) to grid-template css
+//and then create each div
+function populate(size) {
+    container.style.setProperty('--size',size);
+    for (let i=0; i<size*size; i++){
+        const div = document.createElement('div');
+        div.classList.add('pixel');
+        div.addEventListener('mouseover',()=>{
+            if(!draw) return;
+            div.style.backgroundColor = color.value;
+        })
 
-let currentSize=12;
+        div.addEventListener('mousedown',()=>{
+            div.style.backgroundColor = color.value;
+        })
 
-const pixelContainer = document.querySelector('.pixel-container');
-
-let click=0;
-function changeColor(e) {
-
-    if(e.type === "click" && click == 0){
-        click = 1;
+        container.appendChild(div);
     }
-
-    else if(e.type === "click" && click == 1){
-        click = 0;
-    }
-    
-    if(click){
-    e.target.style.background = currentColor;}
 }
 
-
-function makePixel (pixelRow, pixelColumn){
-    
-    const pixel = document.createElement('div');
-    pixel.style.width = `${pixelColumn}px`;
-    pixel.style.height = `${pixelRow}px`;
-    pixel.className = 'pixel';
-    pixel.addEventListener('mouseover',(e)=>{changeColor(e);});
-    pixel.addEventListener('click',(e)=>{changeColor(e);});
-    pixelContainer.appendChild(pixel);
+function redraw(){
+    container.innerHTML = '';
+    populate(size);
 }
 
-function drawGrid(row,column){
-    //let pixelRow = (pixelContainer.clientHeight)/row;
-    //let pixelColumn = (pixelContainer.clientWidth)/column;
-    let pixelRow = 800/row;
-    let pixelColumn = 800/column;
-    for(let i=0; i<(row*column);i++){
-        makePixel(pixelRow,pixelColumn);
-    }
+window.addEventListener("mousedown", ()=>{
+    draw = true;
+})
 
-}
+window.addEventListener("mouseup", ()=>{
+    draw = false;
+})
 
+resetBtn.addEventListener('click', redraw)
 
-drawGrid(20,20);
+sizeInput.addEventListener('input',()=>{
+    size = sizeInput.value;
+    redraw();
+});
 
-
+populate(size);
